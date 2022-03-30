ca391ec3aab196530959b0a7d223d069fbac5832
cxz <2405819294@qq.com>
2022-03-31 07:02:18 +0800

    实现了静态类方法resolve和reject

--- HYPromise.js
+++ HYPromise.js
@@ -61,50 +61,68 @@ class HYPromise {
     }
 
     then(onFulfilled, onRejected) {
-        //如果使用了catch方法，则抛出上一个promise的异常给下一个promise的catch
-        onRejected = onRejected || (err => { throw err })
+            //如果使用了catch方法，则抛出上一个promise的异常给下一个promise的catch
+            onRejected = onRejected || (err => { throw err });
+
+            /*
+            如果在上一个promise中resolve成功了，下一个紧接着是catch(调用rejsct)
+            和finally，之前的写法会让catch无法执行，变成undefined，导致finally也无法执行，
+            因此，如果onFulfilled不存在，则将上一个promise的value返回出去，直到有resolve的调用。
+            */
+            onFulfilled = onFulfilled || (value => { return value });
             /*
             3.实现then的链式调用：return一个promise,前文中无论是resolve还是reject返回
             的值都是作为以一个promise的resolve，除非是reject中throw了一个err，才会使得下一个
             promise变成reject/catch到 。
             */
-        return new HYPromise((resolve, reject) => {
-            //1.如果在then调用时，状态已经确定下来
-            if (this.status === PROMISE_STATUS_FULFILLED && onFulfilled) {
-                execFunctionWithCatchError(onFulfilled, this.value, resolve, reject);
-            }
-            if (this.status === PROMISE_STATUS_REJECTED && onRejected) {
-                execFunctionWithCatchError(onRejected, this.reason, resolve, reject);
-            }
-            /*
-            2.将成功回调和失败回调且保存值传入下一个调用的过程函数存入数组中,
-            保证每个需要被回调的函数返回的值都保存下来并在下一个promise执行
-            */
-            if (onFulfilled) this.onFulfilledFns.push(() => {;
-                execFunctionWithCatchError(onFulfilled, this.value, resolve, reject);
+            return new HYPromise((resolve, reject) => {
+                //1.如果在then调用时，状态已经确定下来
+                if (this.status === PROMISE_STATUS_FULFILLED && onFulfilled) {
+                    execFunctionWithCatchError(onFulfilled, this.value, resolve, reject);
+                }
+                if (this.status === PROMISE_STATUS_REJECTED && onRejected) {
+                    execFunctionWithCatchError(onRejected, this.reason, resolve, reject);
+                }
+                /*
+                2.将成功回调和失败回调且保存值传入下一个调用的过程函数存入数组中,
+                保证每个需要被回调的函数返回的值都保存下来并在下一个promise执行
+                */
+                if (onFulfilled) this.onFulfilledFns.push(() => {;
+                    execFunctionWithCatchError(onFulfilled, this.value, resolve, reject);
+                })
+                if (onRejected) this.onRejectedFns.push(() => {
+                    execFunctionWithCatchError(onRejected, this.reason, resolve, reject);
+                });
             })
-            if (onRejected) this.onRejectedFns.push(() => {
-                execFunctionWithCatchError(onRejected, this.reason, resolve, reject);
-            });
-        })
 
-    }
-
+        }
+        //es6特有方法
     catch (onRejected) {
-        this.then(undefined, onRejected);
+        //catch也返回一个promise
+        return this.then(undefined, onRejected);
+    } finally(onFinally) {
+        //不管成功还是失败，都调用finally函数
+        this.then(() => {
+            onFinally();
+        }, () => {
+            onFinally();
+        });
     }
-}
+    //实现promise的类方法
+    static resolve(value) {
+        return new HYPromise((resolve) => (resolve(value)));
+    }
 
-const promise = new HYPromise((resolve, reject) => {
-    //当前promise的resolve的调用值应该是上一个promise的resolve的返回值
-    // resolve("resolve11111");
-    reject("reject11111")
+    static reject(reason) {
+        return new HYPromise((resolve, reject) => { reject(reason) });
+    }
+
+}
+//测试静态方法
+HYPromise.resolve("hello world").then(res => {
+    console.log(res);
 })
 
-//调用promise的catch方法
-promise.then(res => {
-    console.log("res:", res);
-    return "123";
-}).catch(err => {
-    console.log("err:", err);
+HYPromise.reject("ERR message").then(res => {}, err => {
+    console.log(err);
 })
